1f37752d02fff11f2f8e2b2962befa23674558e0
Erlon <erlonecia@msn.com>
2024-11-26 16:40:50 -0300

    Backend created

---
+++ backend/src/main/java/com/erloneciacomputadores/igreja/IgrejaApplication.java
@@ -0,0 +1,13 @@
+package com.erloneciacomputadores.igreja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgrejaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgrejaApplication.class, args);
+	}
+
+}
